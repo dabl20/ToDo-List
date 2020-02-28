cfa5f81d46b550052f389c68d62a1f508f430ca8
dabl20 <dabl20@protonmail.ch>
2020-02-28 18:42:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 console.log(localStorage);
 var items = []; 
-items = JSON.parse(localStorage["list"]);
 
-for (let i of items) {
-    addItem(i);
-};
-
-function createItem(data) {
-  items.push(data);
-  localStorage["list"] = JSON.stringify(items);
-  addItem(data);
+if (localStorage.length !== 0) {
+    items = JSON.parse(localStorage.list);
+    for (let el of items) {
+        addItem(el);
+    };
 };
 
 function addItem(data){
-  let listItem = document.createElement('li');
-  listItem.innerHTML = `<label><input type="checkbox"><span> ${data} </span></label>`;
-  list.append(listItem);
+    let listItem = document.createElement('li');
+    listItem.innerHTML = `<label id="${data.id}"><input type="checkbox" id="stat"><span> ${data.text} </span></label>`;
+    list.append(listItem);
 };
+
+function Item(data){
+    this.id = items.length;
+    this.text = data;
+    this.checked = false;
+};
+
+function createItem(data) {
+    items.push(new Item(data));
+    localStorage.list = JSON.stringify(items);
+    addItem(data);
+};
+
+console.log(localStorage.list);
