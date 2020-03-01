25b207ed2a82b899e4574587107049f1ac7d1677
dabl20 <dabl20@protonmail.ch>
2020-03-01 23:51:19 +0300

    Update main.js

--- main.js
+++ main.js
@@ -7,43 +7,52 @@ if (localStorage.length !== 0) {
   }
 }
 
-function addItem(data) {
-  let li = document.createElement('li');
-  let stat;
-  if (data.checked) {
-    stat = 'checked';
-  } else {
-    stat = '';
-  };
-  li.innerHTML = `<label id="${data.id}"><input type="checkbox" class="stat" ${stat}><span> ${data.text} </span></label>`;
-  list.append(li);
-}
-
-function Item(data) {
-  this.id = items.length;
-  this.text = data;
-  this.checked = false;
-}
-
 function createItem(data) {
   items.push(new Item(data));
   localStorage.items = JSON.stringify(items);
   addItem(data);
 }
 
-let stat = document.querySelectorAll('.stat');
-stat.forEach(el => {
-  el.addEventListener("click", modifyStat);
-  let id = el.parentNode.getAttribute('id');
+function Item(data) {
+  this.text = data;
+  this.check = false;
+}
 
+function addItem(data) {
+  let li = document.createElement('li');
+  li.setAttribute('id', items.indexOf(data));
+  let stat;
+  if (data.check) {
+    stat = 'checked';
+  } else {
+    stat = '';
+  };
+  li.innerHTML = `<button>DEL</button><label><input type="checkbox" ${stat}><span> ${data.text} </span></label>`;
+  list.append(li);
+}
+
+for (let i = 0; i < list.children.length; i++) {
+  let el = list.children[i].querySelector('input[type="checkbox"]');
+  el.addEventListener('click', modifyStat);
   function modifyStat() {
-    if (el.hasAttribute("checked")) {
-      el.removeAttribute("checked", "");
-      items[id].checked = false;
+    if (el.hasAttribute('checked')) {
+      el.removeAttribute('checked', '');
+      items[i].check = false;
     } else {
-      el.setAttribute("checked", "");
-      items[id].checked = true;
+      el.setAttribute('checked', '');
+      items[i].check = true;
     }
     localStorage.items = JSON.stringify(items);
   }
-})
+}
+
+for (let i = 0; i < list.children.length; i++) {
+  console.log(list.childNodes[i])
+  let el = list.childNodes[i].querySelector('button');
+  el.addEventListener('click', delItem);
+  function delItem() {
+    items.splice(i, 1);
+    localStorage.items = JSON.stringify(items);
+    list.childNodes[i].remove();
+  }
+}
