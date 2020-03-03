587687c79daf33abb1b6dfc6664bd7934ad528f7
dabl20 <dabl20@protonmail.ch>
2020-03-03 06:18:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,58 +1,45 @@
-var items = [];
-
-if (localStorage.length !== 0) {
+document.addEventListener('DOMContentLoaded', function () {
+  let items = [];
   items = JSON.parse(localStorage.items);
   for (let el of items) {
     addItem(el);
   }
-}
 
-function createItem(data) {
-  items.push(new Item(data));
-  localStorage.items = JSON.stringify(items);
-  addItem(data);
-}
+  function Item(data) {
+    this.text = data;
+    this.check = false;
+  }
 
-function Item(data) {
-  this.text = data;
-  this.check = false;
-}
+  document.querySelector('form').addEventListener('submit', function () {
+    let data = textForm.value;
+    items.unshift(new Item(data));
+    localStorage.items = JSON.stringify(items);
+    addItem(data);
+  });
 
-function addItem(data) {
-  let li = document.createElement('li');
-  li.setAttribute('id', items.indexOf(data));
-  let stat;
-  if (data.check) {
-    stat = 'checked';
-  } else {
-    stat = '';
-  };
-  li.innerHTML = `<button>DEL</button><label><input type="checkbox" ${stat}><span> ${data.text} </span></label>`;
-  list.append(li);
-}
-
-for (let i = 0; i < list.children.length; i++) {
-  let el = list.children[i].querySelector('input[type="checkbox"]');
-  el.addEventListener('click', modifyStat);
-  function modifyStat() {
-    if (el.hasAttribute('checked')) {
-      el.removeAttribute('checked', '');
-      items[i].check = false;
+  function addItem(data) {
+    let li, stat;
+    if (data.check) {
+      stat = 'checked';
     } else {
-      el.setAttribute('checked', '');
-      items[i].check = true;
-    }
-    localStorage.items = JSON.stringify(items);
+      stat = '';
+    };
+    li = document.createElement('li');
+    li.setAttribute('id', items.indexOf(data));
+    li.innerHTML = `<button>DEL</button><label><input type="checkbox" ${stat}><span> ${data.text} </span></label>`;
+    list.append(li);
+    li.querySelector('input[type="checkbox"]').addEventListener('change', function() {
+      if(this.checked) {
+          items[items.indexOf(data)].check = true;
+      } else {
+          items[items.indexOf(data)].check = false;
+      }
+      localStorage.items = JSON.stringify(items);
+    });
+    li.querySelector('button').addEventListener('click', function() {
+      list.removeChild(this.parentNode);
+      items.splice(items.indexOf(data), 1);
+      localStorage.items = JSON.stringify(items);
+    });
   }
-}
-
-for (let i = 0; i < list.children.length; i++) {
-  console.log(list.childNodes[i])
-  let el = list.childNodes[i].querySelector('button');
-  el.addEventListener('click', delItem);
-  function delItem() {
-    items.splice(i, 1);
-    localStorage.items = JSON.stringify(items);
-    list.childNodes[i].remove();
-  }
-}
+});
