30495741d2d54da24e3b128dea769760365758c1
dabl20 <dabl20@protonmail.ch>
2020-03-07 02:54:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let items = [];
+document.addEventListener('DOMContentLoaded', function () {
+
+  var items = [];
   if (localStorage.items != undefined) {
     items = JSON.parse(localStorage.items);
-    for (let el of items) {
-      addItem(el);
-    }
+    items.forEach(element => addItem(element));
   }
 
   function Item(data) {
@@ -12,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
     this.check = false;
   }
 
-  document.querySelector('form').addEventListener('submit', function() {
+  document.forms[0].addEventListener('submit', function () {
     let data = document.querySelector('input[type="text"]').value;
     items.unshift(new Item(data));
     localStorage.items = JSON.stringify(items);
@@ -30,18 +29,39 @@ document.addEventListener('DOMContentLoaded', function() {
     li.setAttribute('id', items.indexOf(data));
     li.innerHTML = `<button>DEL</button><label><input type="checkbox" ${stat}><span> ${data.text} </span></label>`;
     document.querySelector('ul').append(li);
-    li.querySelector('input[type="checkbox"]').addEventListener('change', function() {
-      if(this.checked) {
-          items[items.indexOf(data)].check = true;
+    li.querySelector('input[type="checkbox"]').addEventListener('change', function () {
+      if (this.checked) {
+        items[items.indexOf(data)].check = true;
       } else {
-          items[items.indexOf(data)].check = false;
+        items[items.indexOf(data)].check = false;
       }
       localStorage.items = JSON.stringify(items);
     });
-    li.querySelector('button').addEventListener('click', function() {
+    li.querySelector('button').addEventListener('click', function () {
       document.querySelector('ul').removeChild(this.parentNode);
       items.splice(items.indexOf(data), 1);
       localStorage.items = JSON.stringify(items);
     });
   }
+
+  document.querySelector('select').addEventListener('change', (event) => {
+    document.querySelectorAll('input[type="checkbox"]').forEach(element => {
+      if (event.target.value == 'all') {
+        element.parentElement.parentElement.removeAttribute('hidden', '');
+      }
+      if (event.target.value == 'checked') {
+        element.parentElement.parentElement.removeAttribute('hidden', '');
+        if (element.getAttribute('checked') == null) {
+          element.parentElement.parentElement.setAttribute('hidden', '');
+        }
+      }
+      if (event.target.value == 'unchecked') {
+        element.parentElement.parentElement.removeAttribute('hidden', '');
+        if (element.getAttribute('checked') == '') {
+          element.parentElement.parentElement.setAttribute('hidden', '');
+        }
+      }
+    });
+  });
+
 });
